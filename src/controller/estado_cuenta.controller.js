fd51ebaae40122bf43ffc27404cb0733573241a6
Juan Alexis Vidal Gonzales <alex.vidal.gonzales@gmail.com>
2022-02-28 01:08:46 -0500

    master

--- src/controller/estado_cuenta.controller.js
+++ src/controller/estado_cuenta.controller.js
@@ -7,6 +7,7 @@ import sequelize from 'sequelize';
 const Op = Sequelize.Op;
 import Factura from '../models/factura.model';
 import Documento from '../models/documento.model';
+import TipoDocumentoEstado from '../models/tipodocestado.model';
 export async function getFacturas(req, res) {
     const { p_proveedorid } = req.body;
     let sid = "''";
@@ -46,14 +47,20 @@ export async function getFacturas(req, res) {
 };
 
 export async function getDocumentos(req, res) {
-    const { p_proveedorid, p_tipo_documento } = req.body;
+    const { p_proveedorid, p_tipo_documento, p_unidadnegocioid, p_lineaid } = req.body;
     let sid = "''";
     let xp_proveedorid = null;
+    let xp_unidadnegocioid = null;
+    let xp_lineaid = null;
     try {
-        //sid = clienteid.join(",");
-
         if (p_proveedorid != null) {
             xp_proveedorid = "'" + p_proveedorid.join(",") + "'";
+        }
+        if (p_unidadnegocioid != null) {
+            xp_unidadnegocioid = "'" + p_unidadnegocioid.join(",") + "'";
+        }
+        if (p_lineaid != null) {
+            xp_lineaid = "'" + p_lineaid.join(",") + "'";
         }
         console.log(xp_proveedorid)
     } catch (error) {
@@ -62,7 +69,7 @@ export async function getDocumentos(req, res) {
     }
     try {
         let entidades = await Documento.sequelize.query(
-            "SELECT * from fn_docs_tipo_documento(" + xp_proveedorid + ",'" + p_tipo_documento + "')", {
+            "SELECT * from fn_docs_tipo_documento(" + xp_proveedorid + ",'" + p_tipo_documento + "'," + xp_unidadnegocioid + "," + xp_lineaid + ")", {
                 type: Documento.sequelize.QueryTypes.SELECT,
             });
 
@@ -76,6 +83,31 @@ export async function getDocumentos(req, res) {
             });
         }
     } catch (e) {
+        return res.status(500).json({
+            message: 'Algo salio mal',
+            data: {}
+        });
+    }
+};
+
+export async function getTipoDocumentos(req, res) {
+    try {
+
+        let entidades = await TipoDocumentoEstado.sequelize.query(
+            "SELECT * from axeso.fn_get_tipo_documentos_estado();", {
+                type: TipoDocumentoEstado.sequelize.QueryTypes.SELECT,
+            });
+        if (entidades) {
+            return res.status(200).json({
+                data: entidades
+            });
+        } else {
+            return res.status(200).json({
+                data: {}
+            });
+        }
+    } catch (e) {
+        console.log(e.message)
         return res.status(500).json({
             message: 'Algo salio mal',
             data: {}
--- src/routes/estado_cuenta.routes.js
+++ src/routes/estado_cuenta.routes.js
@@ -2,8 +2,10 @@ const { Router } = require('express');
 const router = Router();
 const {
     getFacturas,
-    getDocumentos
+    getDocumentos,
+    getTipoDocumentos
 } = require('../controller/estado_cuenta.controller')
 router.post('/getFacturas', getFacturas);
 router.post('/getDocumentos', getDocumentos);
+router.get('/getTipoDocumentos', getTipoDocumentos);
 module.exports = router;
