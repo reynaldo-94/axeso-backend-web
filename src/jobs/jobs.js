a0d4f3f2260bea389b20236d7d417c1e76796559
Reynaldo <reynaldo.cauche@hatun.com.pe>
2022-06-17 02:16:47 -0500

    jobs: test of insert with date now

--- src/jobs/jobs.js
+++ src/jobs/jobs.js
@@ -53,9 +53,9 @@ export async function jobs() {
         //     { type: Sequelize.QueryTypes.INSERT }
         // );
         await sequelize.query(
-            `INSERT INTO axeso.jobs_detalle(fecha_registro) VALUES (timezone('utc', now()) at time zone 'America/Lima')`,
+            `INSERT INTO axeso.jobs_detalle(fecha_registro) VALUES (timezone('utc', now()))`,
             { type: Sequelize.QueryTypes.INSERT }
-        );s
+        );
         console.log('Insert en la tabla jobs_detalle');
     } catch (e) {
         console.log('Error in Jobs', e)
