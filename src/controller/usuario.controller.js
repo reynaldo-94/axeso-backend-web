6dd77d2bac33623614236ae04aa71a5a34301f00
Reynaldo <reynaldo.cauche@hatun.com.pe>
2022-07-15 12:20:45 -0500

    usuario: Modifcar ruta envio imagenes correo

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -1152,12 +1152,12 @@ export async function testSendEmail(req, res) {
             attachments: [
                 {
                     filename: 'logo-dimexa.jpeg',
-                    path: __dirname + '\\logo-dimexa.jpeg',
+                    path: __dirname + '/logo-dimexa.jpeg',
                     cid: 'logo-dimexa.jpeg' //same cid value as in the html img src
                 },
                 {
                     filename: 'fondo-dimexa.jpeg',
-                    path: __dirname + '\\fondo-dimexa.jpeg',
+                    path: __dirname + '/fondo-dimexa.jpeg',
                     cid: 'fondo-dimexa.jpeg' //same cid value as in the html img src
                 },
     
