688e8cd396698e35c7d1fba987a9bfadb82c9709
Reynaldo <reynaldo.cauche@hatun.com.pe>
2022-08-30 22:53:47 -0500

    update indicadorservicio

--- src/controller/indicadores_servicio.controller.js
+++ src/controller/indicadores_servicio.controller.js
@@ -10,6 +10,79 @@ export async function getIndicadoresServicio(req, res) {
     const {
         p_proveedorid,
         p_anioid
+    } = req.body;   
+
+    if(p_proveedorid === null) {
+        return res.status(200).json("Valor de proveedorid es obligatorio");
+    }
+
+    if(p_anioid === null) {
+        return res.status(200).json("Valor de año es obligatorio");
+    }
+
+    // let entidadesFillRate = await IndicadorServicioFillRate.findAll({
+    //     where: {
+    //         [Op.and]: IndicadorServicioFillRate.sequelize.literal("idproveedor = '" + p_proveedorid + "' AND anio = '" + p_anioid + "'")
+    //     }
+    // }); 
+
+    let entidadesDiasInventario = await Inventario.sequelize.query(
+        "select * from public.inventario limit 3", {
+            type: Inventario.sequelize.QueryTypes.SELECT,
+        });
+    console.log('entidadesDiasInventario', entidadesDiasInventario)
+
+    // let entidadesDiasInventario = await IndicadorServicioDiasInventario.findAll({
+    //     where: {
+    //         [Op.and]: IndicadorServicioDiasInventario.sequelize.literal("idproveedor = '" + p_proveedorid + "' AND anio = '" + p_anioid + "'")
+    //     }
+    // });
+
+    // let entidadesLeadTime = await IndicadorServicioLeadTime.findAll({
+    //     where: {
+    //         [Op.and]: IndicadorServicioLeadTime.sequelize.literal("idproveedor = '" + p_proveedorid + "' AND anio = '" + p_anioid + "'")
+    //     }
+    // });
+
+    // let entidadesInvFueraPlazo = await IndicadorServicioInvFueraPlazo.findAll({
+    //     where: {
+    //         [Op.and]: IndicadorServicioInvFueraPlazo.sequelize.literal("idproveedor = '" + p_proveedorid + "' AND anio = '" + p_anioid + "'")
+    //     }
+    // })        
+
+    const responseFormat = {
+        fill_rate: {
+            table_fill_rate: []
+        },
+        dias_inventario: {
+            table_dias_inventario: entidadesDiasInventario
+        },
+        lead_time: {
+            table_lead_time: []
+        },
+        inventario_fueraplazo: {
+            table_inventario_fueraplazo: []
+        }
+
+    }
+
+    if (entidadesDiasInventario) {
+        return res.status(200).json({
+            data: responseFormat
+        });
+    } else {
+        return res.status(200).json({
+            data: {}
+        });
+    }
+        
+    
+}
+
+export async function getIndicadoresServicio2(req, res) {
+    const {
+        p_proveedorid,
+        p_anioid
     } = req.body;
     try {
 
