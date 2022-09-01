ccbf63eea233a5946789b7d3895d4e109cc90df3
Reynaldo <reynaldo.cauche@hatun.com.pe>
2022-09-01 13:22:33 -0500

    load jobs dashboard

--- src/jobs/jobs.js
+++ src/jobs/jobs.js
@@ -38,7 +38,7 @@ export async function jobs() {
             `delete from axeso.dashboard_sellin_sellout_mensual`,
             { type: Sequelize.QueryTypes.DELETE }
         );
-        console.log('Done Job delete Dashboard SellIn Sellout Mensual', deleteSellinout)
+        console.log('Done Job delete Dashboard SellIn Sellout Mensual', deleteSellinout)        
 
         console.log('Executing load data Sellout Periodo Actual')
         const dataSellout = await sequelize.query(
@@ -59,7 +59,7 @@ export async function jobs() {
             `select * from axeso.fn_cargar_deuda_pendiente(null)`,
             { type: Sequelize.QueryTypes.SELECT }
         );
-        console.log('Done Job Deuda Pendiente', dataDeudaPend)
+        console.log('Done Job Deuda Pendiente', dataDeudaPend)        
 
         console.log('Executing load data Dashboard')
         const dataDashboard = await sequelize.query(
@@ -67,55 +67,12 @@ export async function jobs() {
             { type: Sequelize.QueryTypes.SELECT }
         );
         console.log('Done Job Dashboard', dataDashboard)
-
-        // console.log('Executing load data Alicorp')
-        // const dataIndicadorServicioAlicorp = await sequelize.query(
-        //     `select * from axeso.fn_cargar_indicadorservicio('P103','2022','SI')`,
-        //     { type: Sequelize.QueryTypes.SELECT }
-        // );
-        // console.log('Done Job Alicorp', dataIndicadorServicioAlicorp)
-
-        // console.log('Executing load data Farmin')
-        // const dataIndicadorServicioFarmin = await sequelize.query(
-        //     `select * from axeso.fn_cargar_indicadorservicio('P0719','2022','SI')`,
-        //     { type: Sequelize.QueryTypes.SELECT }
-        // );
-        // console.log('Done Job Farmin', dataIndicadorServicioFarmin)
-
-        // console.log('Executing load data Sellout Periodo Actual')
-        // const dataSellout = await sequelize.query(
-        //     `select * from axeso.fn_cargar_sellout_por_periodo_actual()`,
-        //     { type: Sequelize.QueryTypes.SELECT }
-        // );
-        // console.log('Done Job Sellout Periodo Actual', dataSellout)
-
-        // console.log('Executing load data SellIn')
-        // const dataCompras = await sequelize.query(
-        //     `select * from axeso.fn_cargar_comprasingresos(null)`,
-        //     { type: Sequelize.QueryTypes.SELECT }
-        // );
-        // console.log('Done Job Sellin', dataCompras)
-
-        // console.log('Executing load data Deuda Pendiente')
-        // const dataDeudaPend = await sequelize.query(
-        //     `select * from axeso.fn_cargar_deuda_pendiente(null)`,
-        //     { type: Sequelize.QueryTypes.SELECT }
-        // );
-        // console.log('Done Job Deuda Pendiente', dataDeudaPend)
         
-
-        // console.log('Executing load data Dashboard')
-        // const dataDashboard = await sequelize.query(
-        //     `select * from axeso.fn_cargar_dashboard(null)`,
-        //     { type: Sequelize.QueryTypes.SELECT }
-        // );
-        // console.log('Done Job Dashboard', dataDashboard)
-        
-        // await sequelize.query(
-        //     `INSERT INTO axeso.jobs_detalle(fecha_registro) VALUES (now() - interval '10 hour')`,
-        //     { type: Sequelize.QueryTypes.INSERT }
-        // );
-        // console.log('Insert en la tabla jobs_detalle');
+        await sequelize.query(
+            `INSERT INTO axeso.jobs_detalle(fecha_registro) VALUES (now() - interval '10 hour')`,
+            { type: Sequelize.QueryTypes.INSERT }
+        );
+        console.log('Insert en la tabla jobs_detalle');
     } catch (e) {
         console.log('Error in Jobs', e)
     }
