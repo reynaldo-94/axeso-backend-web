ec36cb217f2f157c66a4e9c61e64a1e9134e0a34
Reynaldo <reynaldo.cauche@hatun.com.pe>
2022-09-01 13:11:30 -0500

    load jobs dashboard

--- src/jobs/jobs.js
+++ src/jobs/jobs.js
@@ -4,19 +4,84 @@ import { sequelize } from '../database/database';
 export async function jobs() {
     console.log('Executing Jobs')
     try {
-        console.log('Executing load data Alicorp')
-        const dataIndicadorServicioAlicorp = await sequelize.query(
-            `select * from axeso.fn_cargar_indicadorservicio('P103','2022','SI')`,
-            { type: Sequelize.QueryTypes.SELECT }
-        );
-        console.log('Done Job Alicorp', dataIndicadorServicioAlicorp)
 
-        console.log('Executing load data Farmin')
-        const dataIndicadorServicioFarmin = await sequelize.query(
-            `select * from axeso.fn_cargar_indicadorservicio('P0719','2022','SI')`,
+        console.log('Executing delete Dashboard Sellout Cobertura')
+        const deleteSellCob = await sequelize.query(
+            `delete from axeso.dashboard_sellout_cobertura`,
+            { type: Sequelize.QueryTypes.DELETE }
+        );
+        console.log('Done Job delete Dashboard Sellout Cobertura', deleteSellCob)
+
+        console.log('Executing delete Dashboard Inventario')
+        const deleteInv = await sequelize.query(
+            `delete from axeso.dashboard_inventario`,
+            { type: Sequelize.QueryTypes.DELETE }
+        );
+        console.log('Done Job delete Dashboard Inventario', deleteInv)
+
+        console.log('Executing delete Dashboard Deuda Pendiente')
+        const deleteDeudPend = await sequelize.query(
+            `delete from axeso.dashboard_deudapendiente`,
+            { type: Sequelize.QueryTypes.DELETE }
+        );
+        console.log('Done Job delete Dashboard Deuda Pendiente', deleteDeudPend)
+
+        console.log('Executing delete Dashboard SellIn')
+        const deleteSellin = await sequelize.query(
+            `delete from axeso.dashboard_sellin`,
+            { type: Sequelize.QueryTypes.DELETE }
+        );
+        console.log('Done Job delete Dashboard SellIn', deleteSellin)
+
+        console.log('Executing delete Dashboard SellIn Sellout Mensual')
+        const deleteSellinout = await sequelize.query(
+            `delete from axeso.dashboard_sellin_sellout_mensual`,
+            { type: Sequelize.QueryTypes.DELETE }
+        );
+        console.log('Done Job delete Dashboard SellIn Sellout Mensual', deleteSellinout)
+
+        console.log('Executing load data Sellout Periodo Actual')
+        const dataSellout = await sequelize.query(
+            `select * from axeso.fn_cargar_sellout_por_periodo_actual()`,
             { type: Sequelize.QueryTypes.SELECT }
         );
-        console.log('Done Job Farmin', dataIndicadorServicioFarmin)
+        console.log('Done Job Sellout Periodo Actual', dataSellout)
+
+        console.log('Executing load data SellIn')
+        const dataCompras = await sequelize.query(
+            `select * from axeso.fn_cargar_comprasingresos(null)`,
+            { type: Sequelize.QueryTypes.SELECT }
+        );
+        console.log('Done Job Sellin', dataCompras)
+
+        console.log('Executing load data Deuda Pendiente')
+        const dataDeudaPend = await sequelize.query(
+            `select * from axeso.fn_cargar_deuda_pendiente(null)`,
+            { type: Sequelize.QueryTypes.SELECT }
+        );
+        console.log('Done Job Deuda Pendiente', dataDeudaPend)
+
+        console.log('Executing load data Dashboard')
+        const dataDashboard = await sequelize.query(
+            `select * from axeso.fn_cargar_dashboard(null)`,
+            { type: Sequelize.QueryTypes.SELECT }
+        );
+        console.log('Done Job Dashboard', dataDashboard)
+
+        // console.log('Executing load data Alicorp')
+        // const dataIndicadorServicioAlicorp = await sequelize.query(
+        //     `select * from axeso.fn_cargar_indicadorservicio('P103','2022','SI')`,
+        //     { type: Sequelize.QueryTypes.SELECT }
+        // );
+        // console.log('Done Job Alicorp', dataIndicadorServicioAlicorp)
+
+        // console.log('Executing load data Farmin')
+        // const dataIndicadorServicioFarmin = await sequelize.query(
+        //     `select * from axeso.fn_cargar_indicadorservicio('P0719','2022','SI')`,
+        //     { type: Sequelize.QueryTypes.SELECT }
+        // );
+        // console.log('Done Job Farmin', dataIndicadorServicioFarmin)
+
         // console.log('Executing load data Sellout Periodo Actual')
         // const dataSellout = await sequelize.query(
         //     `select * from axeso.fn_cargar_sellout_por_periodo_actual()`,
